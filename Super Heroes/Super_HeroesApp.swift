030932a9e5b944414953da5898b4cc70b2cbb0d6
sergio altuzar <sergio.altuzar@gmail.com>
2025-05-17 12:32:33 -0600

    Initial Commit

---
+++ Super Heroes/Super_HeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Super_HeroesApp.swift
+//  Super Heroes
+//
+//  Created by Sergio Altuzar on 17/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Super_HeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
